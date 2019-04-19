df2d8014974706ffa64f932701ba7aee51fb4f85
sdiazben <sara.diaz@upc.edu>
2019-04-19 18:12:41 +0200

    added requests (pending)

--- Labs-solutions/Lab7/Lab7.2/imdb/imdb/spiders/imdbscraper.py
+++ Labs-solutions/Lab7/Lab7.2/imdb/imdb/spiders/imdbscraper.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy.http import Request
 
 class ImdbscraperSpider(scrapy.Spider):
     name = 'imdbscraper'
     allowed_domains = ['www.imdb.com']
-    start_urls = ['https://www.imdb.com/title/tt0096463/fullcredits/'
-                  ]
+    start_urls = ['https://www.imdb.com/title/tt0096463/fullcredits/']
 
     def parse(self, response):
         actors = response.xpath('//table[@class = "cast_list"]//tr').extract()
-        # response.xpath('//td[not(@class)]/a').extract()
 
         for actor in actors:
 
@@ -22,15 +20,30 @@ class ImdbscraperSpider(scrapy.Spider):
             else:
                 role_n = ""
 
-
             if len(actor.split("alt=")) > 1:
                 yield {
 
                     'movie_id': response.xpath('//meta[@property = "pageId"]/@content').extract_first(),
                     'movie_name': response.css('title::text').get().strip().split(' (')[0],
                     'movie_year': int(response.xpath('normalize-space(//span[@class = "nobr"])').extract_first()
-                        .replace("(","").replace(")","")),
+                                      .replace("(", "").replace(")", "")),
                     'actor_name': actor.split("alt=")[1].split("title")[0].replace('"', '').strip(),
                     'actor_id': actor.split("name/")[1].split("/")[0],
                     'role_name': role_n
                 }
+
+                # Create request
+                url = "https://"+self.allowed_domains[0] + actor.split('href="')[1].split('"')[0]
+
+                yield Request(url, callback=self.parse_actor)
+
+    def parse_actor(self, response):
+        print("holi")
+        yield {'birth_date':
+                   response.xpath('//script[@type = "application/ld+json"]').extract_first().split('"birthDate":')[
+                       1].split('"')[1]}
+
+        movies = response.xpath('//div[@class = "filmo-category-section"]/div').extract()[3]
+
+        for movie in movies:
+            yield {'movie_name': movie.split('"year_column">')[1].split("\n")[1], }
