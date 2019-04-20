fd6d310d2e1c0fc0a17bfaf4aec886b320a32cd7
sdiazben <sara.diaz@upc.edu>
2019-04-20 15:32:01 +0200

    generated json 3-4 minutes

--- Labs-solutions/Lab7/Lab7.2/imdb/imdb/spiders/imdbscraper.py
+++ Labs-solutions/Lab7/Lab7.2/imdb/imdb/spiders/imdbscraper.py
@@ -2,6 +2,7 @@
 import scrapy
 from scrapy.http import Request
 
+
 class ImdbscraperSpider(scrapy.Spider):
     name = 'imdbscraper'
     allowed_domains = ['www.imdb.com']
@@ -9,41 +10,48 @@ class ImdbscraperSpider(scrapy.Spider):
 
     def parse(self, response):
         actors = response.xpath('//table[@class = "cast_list"]//tr').extract()
-
+        movie_year = str(
+            response.xpath('normalize-space(//span[@class = "nobr"])').extract_first().replace("(", "").replace(")",
+                                                                                                                ""))
         for actor in actors:
-
-            if len(actor.split("<td>")) > 1:
-                if len(actor.split("<td>")[1].split('character">')[1].split('">')) > 1:
-                    role_n = actor.split("<td>")[1].split("/characters")[1].split(">")[1].split("<")[0]
+            if movie_year.find("198") == 0:
+                if len(actor.split("<td>")) > 1:
+                    if len(actor.split("<td>")[1].split('character">'))>1:
+                        if len(actor.split("<td>")[1].split('character">')[1].split("\n")) > 1:
+                                if len(actor.split("<td>")[1].split('character">')[1].split("\n")[1].split(">")) > 1:
+                                    role_n = actor.split("<td>")[1].split('character">')[1].split("\n")[1].strip().split(">")[1].split("<")[0]
+                                else:
+                                    role_n = actor.split("<td>")[1].split('character">')[1].split("\n")[1].strip()
+                        else:
+                            role_n = actor.split("<td>")[1].split('character">')[1].strip().split(" \n")[0]
                 else:
-                    role_n = actor.split("<td>")[1].split('character">')[1].strip().split(" \n")[0]
-            else:
-                role_n = ""
+                    role_n = ""
 
-            if len(actor.split("alt=")) > 1:
-                yield {
+                if len(actor.split("alt=")) > 1:
+                    yield {
 
-                    'movie_id': response.xpath('//meta[@property = "pageId"]/@content').extract_first(),
-                    'movie_name': response.css('title::text').get().strip().split(' (')[0],
-                    'movie_year': int(response.xpath('normalize-space(//span[@class = "nobr"])').extract_first()
-                                      .replace("(", "").replace(")", "")),
-                    'actor_name': actor.split("alt=")[1].split("title")[0].replace('"', '').strip(),
-                    'actor_id': actor.split("name/")[1].split("/")[0],
-                    'role_name': role_n
-                }
+                        'movie_id': response.xpath('//meta[@property = "pageId"]/@content').extract_first(),
+                        'movie_name': response.css('title::text').get().strip().split(' (')[0],
+                        'movie_year': response.xpath('normalize-space(//span[@class = "nobr"])').extract_first()
+                            .replace("(", "").replace(")", ""),
+                        'actor_name': actor.split("alt=")[1].split("title")[0].replace('"', '').strip(),
+                        'actor_id': actor.split("name/")[1].split("/")[0],
+                        'role_name': role_n
+                    }
 
-                # Create request
-                url = "https://"+self.allowed_domains[0] + actor.split('href="')[1].split('"')[0]
+                    # Create request
+                    url = "https://" + self.allowed_domains[0] + actor.split('href="')[1].split('"')[0]
 
-                yield Request(url, callback=self.parse_actor)
+                    yield response.follow(url, callback=self.parse_actor)
 
     def parse_actor(self, response):
-        print("holi")
-        yield {'birth_date':
-                   response.xpath('//script[@type = "application/ld+json"]').extract_first().split('"birthDate":')[
-                       1].split('"')[1]}
+        # yield {'birth_date':
+        # response.xpath('//script[@type = "application/ld+json"]').extract_first().split('"birthDate":')[1].split('"')[1]}
 
-        movies = response.xpath('//div[@class = "filmo-category-section"]/div').extract()[3]
+        movies = response.xpath('//div[@class = "filmo-category-section"]/div').extract()
+
 
         for movie in movies:
-            yield {'movie_name': movie.split('"year_column">')[1].split("\n")[1], }
+            if str(movie.split('"year_column">')[1].split("\n")[1]).strip().find("198") == 0 and "actor" in str(movie):
+                url = "https://" + self.allowed_domains[0] + movie.split('href="')[1].split('"')[0]
+                yield Request(url, callback=self.parse)
