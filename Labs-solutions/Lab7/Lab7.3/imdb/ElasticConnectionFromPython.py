fd09d64ca7b8f089304264ad9edad27d18515e73
Gabriela Martínez <airamgabriela17@gmail.com>
2019-04-22 11:02:02 +0200

    Update ElasticConnectionFromPython.py

--- Labs-solutions/Lab7/Lab7.3/imdb/ElasticConnectionFromPython.py
+++ Labs-solutions/Lab7/Lab7.3/imdb/ElasticConnectionFromPython.py
@@ -2,8 +2,6 @@ from elasticsearch import Elasticsearch, helpers
 import os
 import uuid
 import json
-import sys
-import glob
 
 ELASTIC_API_URL_HOST = os.environ['ELASTIC_API_URL_HOST']
 ELASTIC_API_URL_PORT = os.environ['ELASTIC_API_URL_PORT']
@@ -14,18 +12,6 @@ es = Elasticsearch(host=ELASTIC_API_URL_HOST,
                    scheme='https',
                    port=ELASTIC_API_URL_PORT,
                    http_auth=(ELASTIC_API_USERNAME, ELASTIC_API_PASSWORD))
-
-# es.index(index='gabi',
-#          doc_type='pruebas',
-#          id=uuid.uuid4(),
-#          body={
-#             "movie_id": "tt0096463",
-#             "movie_name": "Working Girl",
-#             "movie_year": 1988,
-#             "actor_name": "Kevin Spacey",
-#             "actor_id": "nm0000228",
-#             "role_name": "Bob Speck"
-#         })
 
 count = 1
 for file in os.listdir("C:\\Users\\gabim\\PycharmProjects\\elasticsearchScraper"):
@@ -40,8 +26,4 @@ for file in os.listdir("C:\\Users\\gabim\\PycharmProjects\\elasticsearchScraper"
 es.indices.refresh(index='imdb')
 
 if es.indices.exists(index="imdb"):
-    #response = es.search(index="imdb", body={"query":{"match_all":{}}})
     print("idbm index exists!")
-    #print(response)
-
-
